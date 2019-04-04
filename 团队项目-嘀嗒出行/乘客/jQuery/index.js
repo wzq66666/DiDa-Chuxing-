292cfdb58bd4ea6154bda0f1323a492ba6a78a44
wangjie3186594 <1442876291@qq.com>
2019-04-04 11:02:57 +0800

    11:02
    11:02

--- 团队项目-嘀嗒出行/乘客/jQuery/index.js
+++ 团队项目-嘀嗒出行/乘客/jQuery/index.js
@@ -1,59 +1,70 @@
 window.onload=function(){
-    var mySwiper = new Swiper('.swiper-container', {
-        loop: true,
-        autoplay: 3000,
-        // 如果需要分页器
-        pagination: '.swiper-pagination',
-      })
+
+    $('.button .leftFn').on('click',function(){
+        
+    })
+
+
+
+
+
+
+
+//     var mySwiper = new Swiper('.swiper-container', {
+//         loop: true,
+//         autoplay: 3000,
+//         // 如果需要分页器
+//         pagination: '.swiper-pagination',
+//       })
     
-      $('#sliderBtns .lBtn').on('click', function () {
-        var cur = $('.sliderImgs li.active').index();
-        cur--;
-        if (cur < 0) {
-          cur = 2
-        }
-        $('.sliderImgs li').eq(cur).addClass('active').siblings().removeClass('active')
-        $('.sliderText').eq(cur).addClass('active').siblings().removeClass('active')
-        return false;
-      })
-    
-    
-      $('#sliderBtns .rBtn').on('click', function () {
-        autoPlay()
-        return false;
-      })
-      var isHover = true;
-      $('#sliderBtns').hover(function () {
-        isHover = !isHover
-      })
-    
-      function autoPlay () {
-        var cur = $('.sliderImgs li.active').index();
-        cur++;
-        if (cur > 2) {
-          cur = 0
-        }
-        $('.sliderImgs li').eq(cur).addClass('active').siblings().removeClass('active')
-        $('.sliderText').eq(cur).addClass('active').siblings().removeClass('active')
-      }
-    
-      var timer = setInterval(function () {
-    
-        if (isHover) {
-          autoPlay()
-    
-        }
-      }, 3000)
+//       $('#sliderBtns .lBtn').on('click', function () {
+//         var cur = $('.sliderImgs li.active').index();
+//         cur--;
+//         if (cur < 0) {
+//           cur = 2
+//         }
+//         $('.sliderImgs li').eq(cur).addClass('active').siblings().removeClass('active')
+//         $('.sliderText').eq(cur).addClass('active').siblings().removeClass('active')
+//         return false;
+//       })
     
     
-      $('.nav .hover-dropdown').hover(function () {
-        if ($(this).hasClass('hide-ms') && $(window).width() < 758) {
-          return
-        }
-        $('ul', this).stop().slideDown(200);
-        $(this).children('a:first').addClass("hov");
-      }, function () {
-        $('ul', this).stop().slideUp(100);
-        $(this).children('a:first').removeClass("hov");
-      });
+//       $('#sliderBtns .rBtn').on('click', function () {
+//         autoPlay()
+//         return false;
+//       })
+//       var isHover = true;
+//       $('#sliderBtns').hover(function () {
+//         isHover = !isHover
+//       })
+    
+//       function autoPlay () {
+//         var cur = $('.sliderImgs li.active').index();
+//         cur++;
+//         if (cur > 2) {
+//           cur = 0
+//         }
+//         $('.sliderImgs li').eq(cur).addClass('active').siblings().removeClass('active')
+//         $('.sliderText').eq(cur).addClass('active').siblings().removeClass('active')
+//       }
+    
+//       var timer = setInterval(function () {
+    
+//         if (isHover) {
+//           autoPlay()
+    
+//         }
+//       }, 3000)
+    
+    
+//       $('.nav .hover-dropdown').hover(function () {
+//         if ($(this).hasClass('hide-ms') && $(window).width() < 758) {
+//           return
+//         }
+//         $('ul', this).stop().slideDown(200);
+//         $(this).children('a:first').addClass("hov");
+//       }, function () {
+//         $('ul', this).stop().slideUp(100);
+//         $(this).children('a:first').removeClass("hov");
+//       });
 }
